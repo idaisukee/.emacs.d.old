324bbf477b197e6e86ec206c4afdd39a03f8c595
INOUE Daisuke <daisuke@debian>
2015-04-22 21:02:39 +0900

    aaa

--- init.el
+++ init.el
@@ -188,12 +188,12 @@
 
 (setq el-get-dir "~/.emacs.d/elisp/el-get/")
 
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
+;; (unless (require 'el-get nil 'noerror)
+;;   (with-current-buffer
+;;       (url-retrieve-synchronously
+;;        "https://raw.github.com/dimitri/el-get/master/el-get-install.el")
+;;     (goto-char (point-max))
+;;     (eval-print-last-sexp)))
 
-(el-get 'sync)
-(add-to-list 'el-get-recipe-path "~/.emacs.d/recipes")
+;; (el-get 'sync)
+;; (add-to-list 'el-get-recipe-path "~/.emacs.d/recipes")
