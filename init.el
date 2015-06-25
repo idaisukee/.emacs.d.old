817a1b5a72b4b60fa51b1e72e9aef3e18ddd6deb
井上大輔 <hibariyashiki@gmail.com>
2015-06-25 21:50:01 +0900

    installed php-mode.el

--- init.el
+++ init.el
@@ -1,4 +1,8 @@
 (add-to-list 'load-path "~/.emacs.d/")
+(add-to-list 'load-path "~/.emacs.d/site-lisp/")
+
 (require 'keybind-init)
 (require 'misc-init)
 (require 'my-function-init)
+
+(require 'php-mode)
