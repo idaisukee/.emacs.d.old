0f843aa6cda5bcab7ea6da848d37b277c643728c
井上大輔 <hibariyashiki@gmail.com>
2015-06-30 12:32:10 +0900

    added alias-init.el

--- init.el
+++ init.el
@@ -5,16 +5,15 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/")
 (add-to-list 'load-path "~/.emacs.d/init-files/")
 
+(require 'el-get-init)
 
-(setq list-of-files-to-require
+(setq files-to-require
 	  (list
-	   'el-get-init
 	   'keybind-init
 	   'misc-init
 	   'my-function-init
+	   'alias-init
 	   'php-mode))
 
 
-(dolist (file-to-require list-of-files-to-require)
-  (require file-to-require))
-
+(mapcar 'require files-to-require)
