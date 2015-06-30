ed7a23501092d80471b56b91ab799aac2e7ec7a9
井上大輔 <hibariyashiki@gmail.com>
2015-06-30 11:07:35 +0900

    created el-get-init.el

--- init.el
+++ init.el
@@ -1,23 +1,10 @@
-(when load-file-name
-  (setq user-emacs-directory (file-name-directory load-file-name)))
-
-(add-to-list 'load-path (locate-user-emacs-file "el-get/el-get"))
-(unless (require 'el-get nil 'noerror)
-  (with-current-buffer
-      (url-retrieve-synchronously
-       "https://raw.githubusercontent.com/dimitri/el-get/master/el-get-install.el")
-    (goto-char (point-max))
-    (eval-print-last-sexp)))
-
-
-
-(el-get-bundle! f)
 
 (add-to-list 'load-path "~/.emacs.d/")
 (add-to-list 'load-path "~/.emacs.d/site-lisp/")
 
 (setq list-of-files-to-require
 	  (list
+	   'el-get-init
 	   'keybind-init
 	   'misc-init
 	   'my-function-init
