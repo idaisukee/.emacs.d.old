6f3270da50bc379ecde7fe4fd55eae4f698fbe96
INOUE Daisuke <hibariyashiki@gmail.com>
2015-09-01 19:07:57 +0900

    modified whitespace.

--- init-files/whitespace-init.el
+++ init-files/whitespace-init.el
@@ -7,23 +7,25 @@
       '(face tabs tab-mark spaces space-mark newline newline-mark))
 
 (setq whitespace-display-mappings
-      '(
-        (space-mark ?\u3000 [?□])  ; 全角スペース
-;;;        (space-mark ?\u0020 [?\xB7])  ; 半角スペース
-		(space-mark ?\u0020 [?\x5f])  ; 半角スペース
+	  '(
+	  (space-mark ?\u3000 [?□])  ; 全角スペース
+;;;        (space-mark ?\u0020 [?\xB7])  ; 半角スペース -> centerdot
+	  (space-mark ?\u0020 [?\x5f])  ; 半角スペース -> underscore
 ;;;        (newline-mark ?\n   [?$ ?\n]) ; 改行記号
-;;;		(newline-mark ?\n   [?\u2193 ?\n]) ; 改行記号 downarrow
-		(newline-mark ?\n   [?v ?\n]) ; 改行記号 downarrow
-;;;		(tab-mark ?\t [?> ?\t])
-		(tab-mark ?\t [?\xBB ?\xBB ?\xBB ?\xBB])
-        ) )
+;;;		(newline-mark ?\n   [?\u2193 ?\n]) ; 改行記号 -> downarrow
+		(newline-mark ?\n   [?v ?\n]) ; 改行記号 -> v
+;;;		(tab-mark ?\t [?> ?\t]) ; tab -> >
+		(tab-mark ?\t [?\xBB ?-]) ; tab -> left guillemet - 
+	) )
 
 (setq whitespace-space-regexp "\\([\x0020\x3000]+\\)" )
-;正規表現に関する文書。 Emacs Lispには、正規表現リテラルがないことへの言及
-;http://www.mew.org/~kazu/doc/elisp/regexp.html
-;
-;なぜか、全体をグループ化 \(\) しておかないと、うまくマッチしなかった罠
-;
+
+;;;正規表現に関する文書。 Emacs Lispには、正規表現リテラルがないことへの言及
+;;;http://www.mew.org/~kazu/doc/elisp/regexp.html
+;;;
+;;;なぜか、全体をグループ化 \(\) しておかないと、うまくマッチしなかった罠
+;;;
+
 (set-face-foreground 'whitespace-space "DimGray")
 (set-face-background 'whitespace-space 'nil)
 (set-face-bold-p 'whitespace-space t)
