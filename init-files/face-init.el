3c72215fd5ed702fd7ad892b0342d7b4532bd985
INOUE Daisuke <hibariyashiki@gmail.com>
2015-09-02 12:27:00 +0900

    face default height 150

--- init-files/face-init.el
+++ init-files/face-init.el
@@ -11,7 +11,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 151 :width normal :foundry "unknown" :family "Inconsolata"))))
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "white" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 150 :width normal :foundry "unknown" :family "Inconsolata"))))
  '(button ((t (:background "white" :foreground "black" :weight light))))
  '(comint-highlight-prompt ((t (:inherit minibuffer-prompt :foreground "cyan1" :inverse-video t))))
  '(completions-common-part ((t (:inherit default :background "black" :foreground "yellow" :slant normal :weight normal :height 150 :width normal))))
@@ -22,7 +22,7 @@
  '(font-lock-string-face ((t (:foreground "hot pink"))))
  '(font-lock-variable-name-face ((t (:foreground "DodgerBlue1"))))
  '(mode-line ((t (:background "blue violet" :foreground "white"))))
- '(mode-line-buffer-id ((t (:foreground "white"))))
+ '(mode-line-buffer-id ((t (:background "white" :foreground "black"))))
  '(mode-line-inactive ((t (:background "violet" :foreground "white"))))
  '(org-meta-line ((t (:inherit font-lock-comment-face :background "black" :foreground "white"))))
  '(rainbow-delimiters-depth-1-face ((t (:foreground "#00ff00"))))
