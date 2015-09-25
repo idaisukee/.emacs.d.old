bbde31456d239fa05f6ecc6272871b5e9f4cfedb
inoue4567 <inoue4567@gmail.com>
2015-09-26 03:20:11 +0900

    appearance-init: tamed height.

--- init-files/appearance-init.el
+++ init-files/appearance-init.el
@@ -1,8 +1,10 @@
 ;;;(load-theme 'manoj-dark t)
 (load-theme 'deeper-blue t)
-
+(setq height 140)
+(when (eq window-system 'ns)
+	(setq height 200))
 (set-face-attribute 'default nil
 					:family "inconsolata"
-					:height 140)
+					:height height)
 
 (provide 'appearance-init)
