34d58b1278fe98c9e94735ad302ec80502ca5843
inoue4567 <inoue4567@gmail.com>
2015-09-26 19:32:33 +0900

    appearance-init: edited to use Hiragino.

--- init-files/appearance-init.el
+++ init-files/appearance-init.el
@@ -1,10 +1,19 @@
 ;;;(load-theme 'manoj-dark t)
 (load-theme 'deeper-blue t)
 (setq height 140)
-(when (eq window-system 'ns)
-	(setq height 200))
+
 (set-face-attribute 'default nil
-					:family "inconsolata"
-					:height height)
+										:family "inconsolata"
+										:height height)
+
+(when (eq window-system 'ns)
+	(setq height 200)
+
+	;; 日本語をヒラギノ角ゴProNにする
+  (set-fontset-font "fontset-default" 'japanese-jisx0208
+										'("Hiragino Mincho ProN"))
+	;; 半角カナをヒラギノ角ゴProNにする
+  (set-fontset-font "fontset-default" 'katakana-jisx0201
+										'("Hiragino Maru Gothic ProN")))
 
 (provide 'appearance-init)
